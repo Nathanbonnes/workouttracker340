dc2357f70066e3a0469eb25f4a710aa7fc9f0746
Nathan Bonnes <nathanbonnes@gmail.com>
2022-03-01 10:35:57 +0100

    added handlebars

---
+++ dbcon.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user            : 'w0xgi67xzqdmcc7n',
+  password        : 'w08iggaqkid9em19',
+  database        : 'esrelor2d0v08v0v'
+});
+module.exports.pool = pool;
---
+++ main.js
@@ -0,0 +1,40 @@
+/*
+    Uses express, dbcon for database connection, body parser to parse form data
+    handlebars for HTML templates
+*/
+
+var express = require('express');
+var mysql = require('./dbcon.js');
+var bodyParser = require('body-parser');
+
+var app = express();
+var handlebars = require('express-handlebars').create({
+        defaultLayout:'main',
+        });
+
+app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static('public'));
+app.set('view engine', 'handlebars');
+app.set('port', process.argv[2]);
+app.set('mysql', mysql);
+app.use('/people_certs', require('./people_certs.js'));
+app.use('/people', require('./people.js'));
+app.use('/planets', require('./planets.js'));
+app.use('/', express.static('public'));
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
